8f70eabcb0bd2f4a3a5b9ff0ec898be85b42f50a
Ibrahim <ibrahimali05757@gmail.com>
2026-07-22 18:17:48 +0100

    template java.fx application created

---
+++ Frontend/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setScene(new Scene(new Label("JavaFX Minimal App"), 400, 300));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
