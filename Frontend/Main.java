ddd8ab8929aa95fc15b8f117bc37f70c08e19390
Ibrahim <ibrahimali05757@gmail.com>
2026-08-30 21:25:26 +0100

    working email scrape

--- Frontend/Main.java
+++ Frontend/Main.java
@@ -1,12 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        stage.setScene(new Scene(new Label("JavaFX Minimal App"), 400, 300));
+        Button button = new Button("Start Search");
+        stage.setScene(new Scene(button, 400, 300));
         stage.show();
     }
 
